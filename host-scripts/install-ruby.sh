8bc815a67da58fdd3dfe2c9ec3143444307effb3
Luiz Ricardo Vizentini <lrvizentini@gmail.com>
2021-10-24 22:58:46 -0300

    fix

--- host-scripts/install-ruby.sh
+++ host-scripts/install-ruby.sh
@@ -7,13 +7,15 @@ sudo apt-get install -y --no-install-recommends git curl zlib1g-dev build-essent
 
 if ! [ -a ~/.rbenv ]; 
 then 
-    clone git://github.com/sstephenson/rbenv.git ~/.rbenv
+    git clone git://github.com/sstephenson/rbenv.git ~/.rbenv
     echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
     echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
     git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
     echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-    source ~/.bash_profile
+    
+    # reload file
+    . ~/.bash_profile
 
     rbenv install -v 2.7.2
     rbenv global 2.7.2
