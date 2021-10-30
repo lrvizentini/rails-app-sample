91813de562f01d93ffedb43419298e1ac5c95146
Luiz Ricardo Vizentini <lrvizentini@gmail.com>
2021-10-29 21:49:42 -0300

    fix site folder permissions

--- host-scripts/install-webserver.sh
+++ host-scripts/install-webserver.sh
@@ -2,6 +2,10 @@ sudo apt-get install -y nginx
 
 gem install puma
 
-sudo mkdir -p /var/www/sites/blog/dev1
-sudo mkdir -p /var/www/sites/blog/dev2
-sudo mkdir -p /var/www/sites/blog/production
+homedir=/var/www/sites/blog
+sudo mkdir -p $homedir
+
+sudo chown -R $(id -u):$(id -g) $homedir
+sudo mkdir -p $homedir/dev1
+sudo mkdir -p $homedir/dev2
+sudo mkdir -p $homedir/production
