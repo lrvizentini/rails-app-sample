414a50dd9a9d950f5dff3ec324b8dda0a03df38b
Luiz Ricardo Vizentini <lrvizentini@gmail.com>
2021-10-30 00:08:20 -0300

    fix install

--- host-scripts/install-webserver.sh
+++ host-scripts/install-webserver.sh
@@ -4,10 +4,10 @@ gem install puma
 
 homedir=/var/www/sites/blog
 user=ubuntu
-sudo mkdir -p $homedir
+# sudo mkdir -p $homedir
 
 
 sudo chown -R $user $homedir
-mkdir -p $homedir/dev1
-mkdir -p $homedir/dev2
-mkdir -p $homedir/production
+# mkdir -p $homedir/dev1
+# mkdir -p $homedir/dev2
+# mkdir -p $homedir/production
