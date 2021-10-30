1878472ce9037d8ee8ab0cb8c62a894f1a8748d8
Luiz Ricardo Vizentini <lrvizentini@gmail.com>
2021-10-29 22:01:43 -0300

    fix folder creation

--- host-scripts/install-webserver.sh
+++ host-scripts/install-webserver.sh
@@ -3,9 +3,11 @@ sudo apt-get install -y nginx
 gem install puma
 
 homedir=/var/www/sites/blog
+user=ubuntu
 sudo mkdir -p $homedir
 
-sudo chown -R $(id -u):$(id -g) $homedir
-sudo mkdir -p $homedir/dev1
-sudo mkdir -p $homedir/dev2
-sudo mkdir -p $homedir/production
+
+sudo chown -R $user $homedir
+mkdir -p $homedir/dev1
+mkdir -p $homedir/dev2
+mkdir -p $homedir/production
